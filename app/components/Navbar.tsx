729bd489ea8ad13f74bd78e61b713c0790fbc353
Sabrina_Rashid <saashid18@gmail.com>
2025-04-13 20:27:43 +0600

    Navbar design done for large device

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="w-full h-[71px] rounded-full bg-[#030C0733] flex justify-between items-center">
+            <div className='ml-[20px]' >
+                <Image src='/asserts/logo.png' alt='image' width={189} height={35.28} />
+            </div>
+            <div className='flex justify-between items-center text-[#FFFFFF] gap-[40px] text-[16px]'>
+                <h1>Home</h1>
+                <h1>Why choose</h1>
+
+                <h1>About Us?</h1>
+                <h1>Features</h1>
+            </div>
+            <div className='mx-4'>
+                <button className='bg-[#FFFFFF] py-2 px-4 rounded-full text-[16px]'>Request Demo</button>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
